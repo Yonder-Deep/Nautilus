0b4191168df6a64ce4a1ec5b8b82a6a7b7986aba
Abirami Sabbani <abirami.sabbani999@gmail.com>
2021-09-21 11:13:50 -0700

    temperature and heading

--- auv/auv.py
+++ auv/auv.py
@@ -30,9 +30,9 @@ CONNECTION_TIMEOUT = 3
 
 # Encoding headers
 POSITION_DATA = 0b10000
-HEADING_DATA = 0b10001
+HEADING_DATA = 0b0010000
 VOLTAGE_DATA = 0b10010
-TEMP_DATA = 0b10011
+MISC_DATA = 0b010
 MOVEMENT_STAT_DATA = 0b10100
 MISSION_STAT_DATA = 0b10101
 FLOODED_DATA = 0b10110
@@ -40,6 +40,8 @@ DEPTH_DATA = 0b011
 WATER_DEPTH_DATA = 0
 
 DEPTH_ENCODE = DEPTH_DATA << 21
+HEADING_ENCODE = HEADING_DATA << 17
+MISC_ENCODE = MISC_DATA << 21
 
 MAX_TIME = 600
 MAX_ITERATION_COUNT = MAX_TIME / SEND_SLEEP_DELAY / 7
@@ -327,7 +329,7 @@ class AUV_Send(threading.Thread):
                         heading = 0
                         temperature = 0
                         pressure = 0
-
+                        #IMU
                         if self.imu is not None:
                             try:
                                 heading,_,_ = self.imu.read_euler()
@@ -341,10 +343,15 @@ class AUV_Send(threading.Thread):
                                 heading = 0
                                 temperature = 0
                                 #self.radio.write(str.encode("log(\"[AUV]\tAn error occurred while trying to read heading and temperature.\")\n"))
-
+                            split_heading = math.modf(heading)
+                            decimal_heading = int(round(split_heading[0], 2) * 100)
+                            whole_heading = int(split_heading[1])
+                            whole_heading = whole_heading << 7
+                            heading_encode = (HEADING_ENCODE | whole_heading | decimal_heading)
+                            self.radio.write(heading_encode, 3)
+                        #Pressure
                         if self.pressure_sensor is not None:
                             self.pressure_sensor.read()
-
                             # defaults to mbars
                             pressure = self.pressure_sensor.pressure()
                             mbar_to_depth = (pressure-1013.25)/1000 * 10.2
@@ -352,18 +359,24 @@ class AUV_Send(threading.Thread):
                                 mbar_to_depth = 0
                             for_depth = math.modf(mbar_to_depth)
                             # standard depth of 10.2
-                            # # TODO Heading and temperature
                             decimal = int(round(for_depth[0], 1) * 10)
                             whole = int(for_depth[1])
                             whole = whole << 4
                             depth_encode = (DEPTH_ENCODE | whole | decimal)
-                            #log("Pressure Read: " + str(self.pressure_sensor.pressure())
-                            #    + ", whole: " + str((whole >> 4)) + ", decimal: " + str(decimal))  # TODO Heading and temperature
-
-                            # conversion for bars
-                            WATER_DEPTH_DATA = pressure * 10.2
-
                             self.radio.write(depth_encode, 3)
+                        #Temperature radio 
+                        whole_temperature = int(temperature)
+                        sign = 0
+                        if whole_temperature < 0:
+                            sign = 1
+                            whole_temperature *= -1
+                        whole_temperature = whole_temperature << 5
+                        sign = sign << 11
+                        temperature_encode = (MISC_ENCODE | sign | whole_temperature)
+                        self.radio.write(temperature_encode, 3)
+
+
+
                     else:
                         lock.release()
 
