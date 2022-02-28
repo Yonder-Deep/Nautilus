0094caa3bec5c26a0298670956cc247f62cb73ef
Falafel72 <kylehu2000@gmail.com>
2022-02-27 17:31:15 -0800

    added debug statements for IMU

--- auv/api/dive_controller.py
+++ auv/api/dive_controller.py
@@ -41,17 +41,25 @@ class DiveController:
         while depth < to_depth and time.time() < start_time + 60:
             try:
                 depth = self.get_depth()
-                _, pitch,_ = self.imu.read_euler()
-                depth_correction = self.pid_depth.pid(depth)
-                pitch_correction = self.pid_pitch.pid(pitch)
-                front_motor_value = depth_correction - pitch_correction
-                back_motor_value = depth_correction + pitch_correction
-                print("Front Motor Value: {}".format(front_motor_value))
-                print("Back Motor value: {}".format(back_motor_valueback_motor_value))
-                self.mc.update_motor_speeds([0, 0, back_motor_value, front_motor_value])
-                print("Succeeded on way down. Depth is", depth)
+                
             except:
                 print("Failed to read pressure going down")
+                return
+
+            try:
+                _, pitch,_ = self.imu.read_euler()
+            except:
+                print("Failed to read IMU value going down")
+                return
+
+            depth_correction = self.pid_depth.pid(depth)
+            pitch_correction = self.pid_pitch.pid(pitch)
+            front_motor_value = depth_correction - pitch_correction
+            back_motor_value = depth_correction + pitch_correction
+            print("Front Motor Value: {}".format(front_motor_value))
+            print("Back Motor value: {}".format(back_motor_valueback_motor_value))
+            self.mc.update_motor_speeds([0, 0, back_motor_value, front_motor_value])
+            print("Succeeded on way down. Depth is", depth)
 
         self.mc.update_motor_speeds([0, 0, 0, 0])
 
