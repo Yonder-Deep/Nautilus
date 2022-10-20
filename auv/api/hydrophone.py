ee6388a8888a3f75a79b6138d2a89ad83caa20a0
George Wu <97696285+ghwusd@users.noreply.github.com>
2022-10-19 20:42:14 -0700

    updated hydrophone recording module

--- auv/api/hydrophone.py
+++ auv/api/hydrophone.py
@@ -1,20 +1,67 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
-from datetime import datetime
+# import sounddevice as sd
+# from scipy.io.wavfile import write
+# from datetime import datetime
+
+# class Hydrophone:
+#     def __init__(self):
+#         self.fs = 62000 # Sample rate
+#         self.audio_recording = None
+
+#     def start_record(self, recording_seconds):
+#         self.audio_recording = sd.rec(int(recording_seconds * self.fs), samplerate=self.fs, channels=2)
+#         sd.wait()
+#         self.save_recording()
+
+#     def save_recording(self):
+#         write(f'{datetime.now().isoformat()}.wav', self.fs, self.audio_recording)  # Save as WAV file 
+
+#     def get_most_recent_recording(self):
+#         return self.audio_recording
+
+import pyaudio
+import wave
 
 class Hydrophone:
     def __init__(self):
-        self.fs = 62000 # Sample rate
-        self.audio_recording = None
+        pass
 
-    def start_record(self, recording_seconds):
-        self.audio_recording = sd.rec(int(recording_seconds * self.fs), samplerate=self.fs, channels=2)
-        sd.wait()
-        self.save_recording()
+    def run(self):
+        chunk = 1024  # Record in chunks of 1024 samples
+        sample_format = pyaudio.paInt16  # 16 bits per sample
+        channels = 2
+        fs = 62100  # Record at 44100 samples per second
+        seconds = 3
+        filename = "output.wav"
 
-    def save_recording(self):
-        write(f'{datetime.now().isoformat()}.wav', self.fs, self.audio_recording)  # Save as WAV file 
+        p = pyaudio.PyAudio()  # Create an interface to PortAudio
 
-    def get_most_recent_recording(self):
-        return self.audio_recording
+        print('Recording')
 
+        stream = p.open(format=sample_format,
+                        channels=channels,
+                        rate=fs,
+                        frames_per_buffer=chunk,
+                        input=True)
+
+        frames = []  # Initialize array to store frames
+
+        # Store data in chunks for 3 seconds
+        for i in range(0, int(fs / chunk * seconds)):
+            data = stream.read(chunk)
+            frames.append(data)
+
+        # Stop and close the stream 
+        stream.stop_stream()
+        stream.close()
+        # Terminate the PortAudio interface
+        p.terminate()
+
+        print('Finished recording')
+
+        # Save the recorded data as a WAV file
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(channels)
+        wf.setsampwidth(p.get_sample_size(sample_format))
+        wf.setframerate(fs)
+        wf.writeframes(b''.join(frames))
+        wf.close()
--- auv/startAudioRecording.py
+++ auv/startAudioRecording.py
@@ -1,4 +1,5 @@
 from api import Hydrophone
 
 hydrophone = Hydrophone()
-hydrophone.start_record(5)
+# hydrophone.start_record(5)
+hydrophone.run()
