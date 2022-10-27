1c70b79003dbbe163dfd5cd254733c4319a4c18b
George Wu <97696285+ghwusd@users.noreply.github.com>
2022-10-26 19:30:33 -0700

    updating audio recording

--- auv/api/hydrophone.py
+++ auv/api/hydrophone.py
@@ -1,68 +1,87 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
-from datetime import datetime
-
-
-class Hydrophone:
-    def __init__(self):
-        self.fs = 62000  # Sample rate
-        self.audio_recording = None
-
-    def start_record(self, recording_seconds):
-        self.audio_recording = sd.rec(int(recording_seconds * self.fs), samplerate=self.fs, channels=2)
-        sd.wait()
-        self.save_recording()
-
-    def save_recording(self):
-        write(f'{datetime.now().isoformat()}.wav', self.fs, self.audio_recording)  # Save as WAV file
-
-    def get_most_recent_recording(self):
-        return self.audio_recording
-
-# import pyaudio
-# import wave
+# from traceback import format_exc
+# from static import global_vars
+# import sounddevice as sd
+# from scipy.io.wavfile import write
+# from datetime import datetime
+# from static import constants
 
 # class Hydrophone:
 #     def __init__(self):
-#         pass
+#         self.fs = 62000  # Sample rate
+#         self.audio_recording = None
 
-#     def run(self):
-#         chunk = 1024  # Record in chunks of 1024 samples
-#         sample_format = pyaudio.paInt16  # 16 bits per sample
-#         channels = 2
-#         fs = 62100  # Record at 44100 samples per second
-#         seconds = 3
-#         filename = "output.wav"
+#     def start_record(self, recording_seconds):
+#         self.audio_recording = sd.rec(int(recording_seconds * self.fs), samplerate=self.fs, channels=2)
+#         sd.wait()
+#         self.save_recording()
 
-#         p = pyaudio.PyAudio()  # Create an interface to PortAudio
+#     def save_recording(self):
+#         write(f'{self.get_new_audio_file_name()}', self.fs, self.audio_recording)  # Save as WAV file
 
-#         print('Recording')
+#     def get_new_audio_file_name(self):
+#         unformated_time_stamp = datetime.now().isoformat().split(":")
+#         formated_time_stamp = ""
+#         for part in unformated_time_stamp:
+#             formated_time_stamp+=part+"."
+#         filename = constants.AUDIO_FOLDER_PATH + formated_time_stamp + "wav"
+#         return filename
 
-#         stream = p.open(format=sample_format,
-#                         channels=channels,
-#                         rate=fs,
-#                         frames_per_buffer=chunk,
-#                         input=True)
+#     def get_most_recent_recording(self):
+#         return self.audio_recording
 
-#         frames = []  # Initialize array to store frames
+#     def record(self):
+#         try:
+#             with sf.SoundFile(self.filepath,
+#                                         mode='x', samplerate=self.SAMPLE_RATE,
+#                                         channels=self.CHANNELS, subtype=None) as file:
+#                 with sd.InputStream(samplerate=self.SAMPLE_RATE, device=self.mic_id,
+#                                             channels=self.CHANNELS, callback=self.callback):
+#                     global_vars.log(f"New recording started: {self.sound_file.name}")
+#                     try:
+#                         while True:
+#                             file.write(self.mic_queue.get())
+#                     except RuntimeError as re:
+#                         global_vars.log(f"{re}. If recording was stopped by the user, then this can be ignored")
+#         except:
+#             global_vars.log(f"Recording failed")
 
-#         # Store data in chunks for 3 seconds
-#         for i in range(0, int(fs / chunk * seconds)):
-#             data = stream.read(chunk)
-#             frames.append(data)
 
-#         # Stop and close the stream
-#         stream.stop_stream()
-#         stream.close()
-#         # Terminate the PortAudio interface
-#         p.terminate()
+# based on arbitrary duration example
+import queue, threading, sys, time
+import sounddevice as sd
+import soundfile as sf
+import numpy  # Make sure NumPy is loaded before it is used in the callback
+assert numpy  # avoid "imported but unused" message (W0611)
 
-#         print('Finished recording')
+class Hydrophone:
+    def __init__(self):
 
-#         # Save the recorded data as a WAV file
-#         wf = wave.open(filename, 'wb')
-#         wf.setnchannels(channels)
-#         wf.setsampwidth(p.get_sample_size(sample_format))
-#         wf.setframerate(fs)
-#         wf.writeframes(b''.join(frames))
-#         wf.close()
+        self.f = "/tmp/rec_threading.wav"
+
+        self.subtype = 'PCM_16'
+        self.dtype = 'int16' 
+
+        self.q = queue.Queue()
+        self.recorder = False
+
+    def rec(self):
+        with sf.SoundFile(self.f, mode='w', samplerate=44100, 
+                        subtype=self.subtype, channels=1) as file:
+            with sd.InputStream(samplerate=44100.0, dtype=self.dtype, 
+                                channels=1, callback=self.save):
+                while getattr(self.recorder, "record", True):
+                    file.write(self.q.get())
+
+    def save(self, indata, frames, time, status):
+        self.q.put(indata.copy())
+
+    def start(self):
+        
+        self.recorder = threading.Thread(target=self.rec)
+        self.recorder.record = True
+        self.recorder.start()
+
+    def stop(self):
+        self.recorder.record = False
+        self.recorder.join()
+        self.recorder = False
--- auv/startAudioRecording.py
+++ auv/startAudioRecording.py
@@ -1,5 +1,14 @@
 from api import Hydrophone
+import time
 
 hydrophone = Hydrophone()
-hydrophone.start_record(5)
-# hydrophone.run()
+
+print('start recording')
+hydrophone.start()
+time.sleep(1)
+print('...still recording...')
+time.sleep(1)
+hydrophone.stop()
+print('stop recording')
+
+
