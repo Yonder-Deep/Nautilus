df510e71a15a829b183d0599436785147795cfc5
George Wu <97696285+ghwusd@users.noreply.github.com>
2022-11-09 20:12:28 -0800

    updated bs radio global var

--- base_station/threads/base_station_send_ping.py
+++ base_station/threads/base_station_send_ping.py
@@ -10,8 +10,7 @@ from static import global_vars
 
 
 class BaseStation_Send_Ping(threading.Thread):
-    def __init__(self, radio, out_q=None):
-        self.radio = radio
+    def __init__(self, out_q=None):
         self.out_q = out_q
         threading.Thread.__init__(self)
 
@@ -26,16 +25,14 @@ class BaseStation_Send_Ping(threading.Thread):
         while True:
             time.sleep(constants.PING_SLEEP_DELAY)
 
-            if self.radio is None or self.radio.is_open() is False:
-                print("TEST radio not connected")
+            if global_vars.radio is None or global_vars.radio.is_open() is False:
                 global_vars.connect_to_radio(self.out_q)
-                self.radio = global_vars.radio
             else:
                 try:
                     # Always send a connection verification packet
                     if not global_vars.downloading_file:
                         constants.radio_lock.acquire()
-                        self.radio.write(constants.PING)
+                        global_vars.radio.write(constants.PING)
                         constants.radio_lock.release()
 
                 except Exception as e:
