040e15a47daa76ca7cd982f29787449d76b51061
Clair Ma <clairbma@gmail.com>
2022-11-20 13:00:25 -0800

    fix file send method

--- auv/threads/auv_send_data.py
+++ auv/threads/auv_send_data.py
@@ -154,7 +154,7 @@ class AUV_Send_Data(threading.Thread):
         filename = [f for f in os.listdir(constants.LOG_FOLDER_PATH) if os.path.isfile(os.path.join(constants.LOG_FOLDER_PATH, f))][0]
         filepath = constants.LOG_FOLDER_PATH + filename
         global_vars.radio.write_data(os.path.getsize(filepath), constants.FILE_SEND_PACKET_SIZE)   # Send size of log file
-        global_vars.radio.write_data(filename, constants.FILE_SEND_PACKET_SIZE)    # Send name of log file
+        global_vars.radio.write(filename, constants.FILE_SEND_PACKET_SIZE)    # Send name of log file
         constants.RADIO_LOCK.release()
         # Start sending contents of file
         dive_log = open(filepath, "rb")
@@ -187,7 +187,7 @@ class AUV_Send_Data(threading.Thread):
         filename = [f for f in os.listdir(constants.AUDIO_FOLDER_PATH) if os.path.isfile(os.path.join(constants.AUDIO_FOLDER_PATH, f))][0]
         filepath = constants.AUDIO_FOLDER_PATH + filename
         global_vars.radio.write_data(os.path.getsize(filepath), constants.FILE_SEND_PACKET_SIZE)   # Send size of audio file
-        global_vars.radio.write_data(filename, constants.FILE_SEND_PACKET_SIZE)    # Send name of audio file
+        global_vars.radio.write(filename, constants.FILE_SEND_PACKET_SIZE)    # Send name of audio file
         # Start sending contents of file
         audio_file = open(filepath, "rb")
         file_bytes = audio_file.read(constants.FILE_SEND_PACKET_SIZE)
