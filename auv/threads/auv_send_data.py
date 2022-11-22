d309f4f1f00b9a31d4dea608a92b2454b861709e
YonderDeep Base Station 2 <deep.yonderdynamics@gmail.com>
2022-11-21 20:35:29 -0800

    no

--- auv/threads/auv_send_data.py
+++ auv/threads/auv_send_data.py
@@ -170,6 +170,8 @@ class AUV_Send_Data(threading.Thread):
             constants.RADIO_LOCK.release()
             # Ensure that base station is receiving every packet sent
             while global_vars.file_packets_sent != global_vars.file_packets_received:
+                print(f"files sent: {global_vars.file_packets_sent}, files received: {global_vars.file_packets_received}")
+
                 if global_vars.bs_response_sent == True:
                     global_vars.bs_response_sent = False
                     constants.RADIO_LOCK.acquire()
