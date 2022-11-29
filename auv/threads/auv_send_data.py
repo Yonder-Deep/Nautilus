e3950208d20ff17237c44d144a358faf20eaae0f
YonderDeep Base Station 2 <deep.yonderdynamics@gmail.com>
2022-11-28 20:55:05 -0800

    please help

--- auv/threads/auv_send_data.py
+++ auv/threads/auv_send_data.py
@@ -189,7 +189,7 @@ class AUV_Send_Data(threading.Thread):
 
     def send_audio_file(self):
         constants.RADIO_LOCK.acquire()
-        global_vars.radio.write(constants.DOWNLOAD_AUDIO_ENCODE, 3)
+        global_vars.radio.write(constants.DOWNLOAD_LOG_ENCODE, 3)
         filename = [f for f in os.listdir(constants.AUDIO_FOLDER_PATH) if os.path.isfile(os.path.join(constants.AUDIO_FOLDER_PATH, f))][0]
         filepath = constants.AUDIO_FOLDER_PATH + filename
         global_vars.radio.write_data(os.path.getsize(filepath), constants.FILE_SEND_PACKET_SIZE)   # Send size of audio file
