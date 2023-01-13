821f90523a00d254e192db5c3eb30382192e2bba
YonderDeep Base Station 2 <deep.yonderdynamics@gmail.com>
2023-01-12 19:53:16 -0800

    update send data

--- auv/threads/auv_send_data.py
+++ auv/threads/auv_send_data.py
@@ -62,7 +62,7 @@ class AUV_Send_Data(threading.Thread):
                         self.send_positioning()
                     elif global_vars.connected and global_vars.sending_data:
                         constants.LOCK.release()
-                        self.send_audio_file()
+                        self.send_dive_log()
                     else:
                         constants.LOCK.release()
 
