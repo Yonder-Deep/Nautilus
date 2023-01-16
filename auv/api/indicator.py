f391804a69c17ddc672929b99f0908e29f208c43
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2023-01-15 16:18:02 -0800

    update LED timing

--- auv/api/indicator.py
+++ auv/api/indicator.py
@@ -9,6 +9,6 @@ class Indicator:
         GPIO.setup(18, GPIO.OUT)
         print("LED on")
         GPIO.output(18, GPIO.HIGH)
-        time.sleep(1000)
+        time.sleep(5)
         print("LED off")
         GPIO.output(18, GPIO.LOW)
