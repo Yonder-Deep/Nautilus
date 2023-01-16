4a29a219980742b73ddc88785f6bb1081e3e6a8d
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2023-01-15 16:27:38 -0800

    change gpio pin led

--- auv/api/indicator.py
+++ auv/api/indicator.py
@@ -6,9 +6,9 @@ class Indicator:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(18, GPIO.OUT)
+        GPIO.setup(12, GPIO.OUT)
         print("LED on")
         GPIO.output(18, GPIO.HIGH)
-        time.sleep(5)
+        time.sleep(10)
         print("LED off")
-        GPIO.output(18, GPIO.LOW)
+        GPIO.output(12, GPIO.LOW)
