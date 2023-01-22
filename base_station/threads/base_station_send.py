35cfc6a860755a22d6b421c27176f0e8394bf824
YonderDeep Base Station 2 <deep.yonderdynamics@gmail.com>
2023-01-22 14:13:58 -0800

    removing nested acquire release in base station send

--- base_station/threads/base_station_send.py
+++ base_station/threads/base_station_send.py
@@ -313,12 +313,10 @@ class BaseStation_Send(threading.Thread):
                             self.out_q.put("set_xbox_status(0,0)")
                             xbox_input = False
                     elif global_vars.connected and global_vars.downloading_file:
-                        constants.radio_lock.acquire()
                         if global_vars.packet_received:
                             print("packet_received")
                             self.send_packet_num()
                             global_vars.packet_received = False
-                        constants.radio_lock.release()
                     else:
                         constants.lock.release()
                 except Exception as e:
