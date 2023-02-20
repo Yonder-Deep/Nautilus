758654512eade66dea9f8f68d8b41294f835ed6d
YonderDeep Base Station 2 <deep.yonderdynamics@gmail.com>
2023-02-19 19:22:09 -0800

    fix Adafruit IMU import; IMU now gives data

--- auv/api/imu.py
+++ auv/api/imu.py
@@ -1,6 +1,7 @@
 # Custom imports
 import serial
-from Adafruit_Python_BNO055.BNO055 import BNO055 as super_imu
+from Adafruit_BNO055.BNO055 import BNO055 as super_imu
+IMU_RESET_PIN = 8
 
 
 class IMU(super_imu):
@@ -8,6 +9,5 @@ class IMU(super_imu):
 
     def __init__(self, path):
         """ Simply call our superclass constructor """
-        super().__init__(serial.Serial(path))
+        super().__init__(serial.Serial(path), rst=IMU_RESET_PIN)
     # TODO Implement more useful functions other than the default
-
