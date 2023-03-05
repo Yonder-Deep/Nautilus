eb45c7afa8b60ba82d7cebebb9c63b1d14b238ed
Yashil Vora <yashil2004@gmail.com>
2023-03-05 15:43:41 -0800

    [ADDED] code for gopro taking videos and photos added

--- auv/api/gopro.py
+++ auv/api/gopro.py
@@ -1,24 +1,41 @@
-
-import cv2
-from time import time
-import socket
 from goprocam import GoProCamera, constants
+import shutil
+import time
 
-gopro = GoProCamera.GoPro(constants.gpcontrol)
-# gopro.overview()
-gopro.stream("udp://127.0.0.1:10000", quality="high")
+go_pro = GoProCamera.GoPro()
+
+# shows everything on the camera: go_pro.listMedia(True)
+
+# to delete all: go_pro.delete("all")
+# # to delete last: go_pro.delete("last")
 
 
-# The camera is available on IP 10.5.5.9, with the MAC address AA:BB:CC:DD:EE:FF.
+def take_photo_transfer_delete():
+    go_pro.take_photo(timer=5)
+    # go_pro.downloadLastMedia(custom_filename="yonderpic.JPG")
+    # go_pro.delete("last")
 
 
-# cap = cv2.VideoCapture("udp://127.0.0.1:10000")
-# while True:
-#     ret, frame = cap.read()
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     cv2.imshow("GoPro OpenCV", frame)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
+def timelapse(interval):
+    while True:
+        go_pro.downloadLastMedia(go_pro.take_photo(timer=interval))
 
-# cap.release()
-# cv2.destroyAllWindows()
+
+def media_download_and_transfer_and_delete():
+    media = go_pro.downloadAll()
+    for i in media:
+        shutil.move('./100GOPRO-{}'.format(i), './gopro_images/100GOPRO-{}'.format(i))
+    # go_pro.delete("all")
+
+# The parameter time must be in seconds
+
+
+def take_video(record_time):
+    # go_pro.getWebcamPreview()
+    go_pro.shoot_video(record_time)
+
+
+take_video(5)
+# take_photo_transfer_delete()
+
+# go_pro.overview()
