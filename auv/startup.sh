e65b361bd9a59b50226b10adc72083f539a44d5b
YonderDeep Base Station 2 <deep.yonderdynamics@gmail.com>
2023-04-17 19:03:46 -0700

    updated evo-settings

--- auv/startup.sh
+++ auv/startup.sh
@@ -2,4 +2,5 @@
 echo "*** IF first time running startup.sh, exit and start again. ***"
 sudo killall pigpiod
 sudo pigpiod
+sudo python -B ./dev/Nautilus/auv/api/evo-settings.py
 sudo python3 -B ./dev/Nautilus/auv/auv.py
