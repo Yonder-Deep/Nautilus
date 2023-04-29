4c783bea3993f6cea5e3611fe623b395cbc9a7c8
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2023-04-28 18:11:24 -0700

    stoof

--- base_station/gui/map_projection.py
+++ base_station/gui/map_projection.py
@@ -1,7 +1,27 @@
+import io
+from urllib.request import urlopen, Request
+from PIL import Image
 import cartopy.feature as cf
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import cartopy.io.img_tiles as cimgt
+
+
+def image_spoof(self, tile):
+    '''this function reformats web requests from OSM for cartopy
+    Heavily based on code by Joshua Hrisko at:
+        https://makersportal.com/blog/2020/4/24/geographic-visualizations-in-python-with-cartopy'''
+
+    url = self._image_url(tile)                # get the url of the street map API
+    req = Request(url)                         # start request
+    req.add_header('User-agent', 'Anaconda 3')  # add user agent to request
+    fh = urlopen(req)
+    im_data = io.BytesIO(fh.read())            # get image
+    fh.close()                                 # close url
+    img = Image.open(im_data)                  # open image with PIL
+    img = img.convert(self.desired_tile_form)  # set image format
+    return img, self.tileextent(tile), 'lower'  # reformat for cartopy
+
 
 crs = ccrs.PlateCarree()
 #crs = ccrs.Geodetic()
@@ -9,12 +29,14 @@ crs = ccrs.PlateCarree()
 # Now we will create axes object having specific projection
 plt.figure(dpi=150)
 
-request = cimgt.OSM()
-projection = ccrs.LambertAzimuthalEqualArea(central_longitude=15, central_latitude=80)
-#projection = request.crs
+cimgt.QuadtreeTiles.get_image = image_spoof  # reformat web request for street map spoofing
+img = cimgt.QuadtreeTiles()  # spoofed, downloaded street map
+
+#projection = ccrs.NorthPolarStereo(central_longitude=15)
+projection = img.crs
 
 ax = plt.axes(projection=projection, frameon=True)
-tile_image = ax.add_image(request, 3)
+tile_image = ax.add_image(img, 3)
 
 # Draw gridlines in degrees over Mercator map
 gl = ax.gridlines(crs=crs, draw_labels=True,
