b54a18a8badbfe13c1c80fe285313218b0141934
DaPhysikist <therealpranavmehta@gmail.com>
2023-04-29 13:13:51 -0700

    moar groundwork

--- auv/api/autonomous_nav.py
+++ auv/api/autonomous_nav.py
@@ -26,6 +26,25 @@ class Autonomous_Nav(threading.Thread):
     def set_curr_position(self, latitude, longitude):
         self.curr_latitude, self.curr_longitude = latitude, longitude
 
+    def path_find(self):
+        searching = True
+        direction_flag = 0
+        while(searching):
+            if direction_flag == 0:
+                if True:
+                    break
+                else:
+                    direction_flag = 1
+                    continue
+            elif direction_flag == 1:
+                if True:
+                    break
+                else:
+                    direction_flag = 0
+                    continue
+            if True:
+                searching = False
+
     def run(self, dest_lat, dest_long):
         self.set_destination(dest_lat, dest_long)
         pass
