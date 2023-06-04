8980952f9fda3eafaa01d78063530f7eceafe990
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2023-06-04 16:51:20 -0700

    maybe

--- auv/api/dive_controller.py
+++ auv/api/dive_controller.py
@@ -89,7 +89,8 @@ class DiveController:
             print("Current time elapsed: {}".format(time.time() - start_time))
 
             # NOTE: check side_motor_value to see if the sign is correct
-            self.mc.update_motor_speeds([min(side_motor_value,100), max(-side_motor_value,-100), min(back_motor_value,100), min(front_motor_value,100)])
+            #self.mc.update_motor_speeds([min(side_motor_value,100), max(-side_motor_value,-100), min(back_motor_value,100), min(front_motor_value,100)])
+            self.mc.update_motor_speeds([side_motor_value, (-1 * side_motor_value), back_motor_value, front_motor_value])
 
             if self.pid_depth.within_tolerance and not target_met:
                 # want to wait for dive_length seconds before stopping
