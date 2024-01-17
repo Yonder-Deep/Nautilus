f020f036f69eef2ae6da9b53ccf8b4d5f89e3560
ttai2023 <89951129+ttai2023@users.noreply.github.com>
2024-01-16 20:31:20 -0800

    Delete auv/missions/D*testFile.py

--- auv/missions/D*testFile.py
+++
@@ -1,5 +0,0 @@
-#calculates distance based on current position, move, update movement 
-#takes fake gps coordinates, feed end point, add obstacles, walls
-#create test file with repo shared
-#how we can implement gps data from that 
-
