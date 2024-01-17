f79d2195677798d0a93afff5bd0960860007a099
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2024-01-16 20:36:05 -0800

    Made more quality fixes to gps.

--- auv/threads/gps.py
+++ auv/threads/gps.py
@@ -10,22 +10,27 @@ class GPS(threading.Thread):
 
     def __init__(self, out_queue):
         threading.Thread.__init__(self)
-
+        path_found = False
         for gps_path in GPS_PATHS:
             try:
                 uart = serial.Serial(gps_path, baudrate=9600, timeout=10)
+                path_found = True
                 break
             except:
                 pass
 
-        self.gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
-        self.gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
-        self.gps.send_command(b"PMTK220,1000")
-        self.out_q = out_queue
-        self.run()
+        if path_found is True:
+            self.gps = adafruit_gps.GPS(uart, debug=False)  # Use UART/pyserial
+            self.gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+            self.gps.send_command(b"PMTK220,1000")
+            self.out_q = out_queue
+            self.running = True
+            self.run()
+        else:
+            raise ("No gps path found.")
 
     def run(self):
-        while True:
+        while self.running is True:
             if not self.gps.has_fix:
                 self.out_q.put(
                     {
@@ -44,3 +49,6 @@ class GPS(threading.Thread):
                         "longitude": self.gps.longitude,
                     }
                 )
+
+    def stop(self):
+        self.running = False
