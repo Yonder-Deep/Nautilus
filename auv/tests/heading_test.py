e7422a2743358937a966579049113d5be4b40218
abijitj <abijit.jay@gmail.com>
2024-02-04 13:57:34 -0800

    Add MotorController to heading_test.py

--- auv/tests/heading_test.py
+++ auv/tests/heading_test.py
@@ -39,27 +39,29 @@ class Heading_Test(threading.Thread):
             d=constants.D_HEADING,
         )
 
-    def update_motor(self) -> None:
+    def update_motor(self, last_speed: float) -> None:
         "Update motor speed with PID control input"
         curr_heading, roll, pitch = self.imu.read_euler()
         pid_input = self.heading_pid.pid_heading(curr_heading)
-        # self.mc.update_motor_speeds([0,turn_speed,0,0])
-        self.motors[TURN_MOTOR_IDX] += pid_input
+
+        self.mc.update_motor_speeds([0,last_speed + pid_input,0,0])
+        #self.motors[TURN_MOTOR_IDX] += pid_input
 
     def run(self, target_heading: float = 0) -> None:
         "Function that conducts the test"
 
         self.heading_pid.update_target(target_heading)  # set target heading
 
-        self.motors[FORWARD_MOTOR_IDX] = 0  # reset all motors to 0 speed
-        self.motors[TURN_MOTOR_IDX] = 0
-        self.motors[BACK_MOTOR_IDX] = 0
-        self.motors[FRONT_MOTOR_IDX] = 0
+        self.motors[FORWARD_MOTOR_IDX].set_speed(0)  # reset all motors to 0 speed
+        self.motors[TURN_MOTOR_IDX].set_speed(0)
+        self.motors[BACK_MOTOR_IDX].set_speed(0)
+        self.motors[FRONT_MOTOR_IDX].set_speed(0)
 
         curr_heading, roll, pitch = self.imu.read_euler()  # read current heading
 
         # start the motor at some speed (maybe MAX_SPEED constant)
         self.motors[TURN_MOTOR_IDX] = 1
+        last_speed = 1
 
         if curr_heading != target_heading:
             reached_target = False
@@ -67,14 +69,14 @@ class Heading_Test(threading.Thread):
         while not reached_target:
             # update_motor()
             # update current_heading
-            self.update_motor()
+            last_speed = self.update_motor(last_speed)
             curr_heading, roll, pitch = self.imu.read_euler()  # read current heading
             # check if current_heading is north/0 for 5 seconds. If so, break
 
             if curr_heading == target_heading:
                 end_time = time.time() + 5
                 while time.time() < end_time:
-                    self.update_motor()
+                    last_speed = self.update_motor(last_speed)
                 new_current_heading, roll, pitch = self.imu.read_euler()
                 if new_current_heading == 0:
                     reached_target = True
