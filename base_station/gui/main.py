0a285f5a40fad2dc58e4bd1c87a7f92c22c1a1b1
abijitj <abijit.jay@gmail.com>
2024-02-04 13:26:16 -0800

    Add heading_test button to GUI

--- base_station/gui/main.py
+++ base_station/gui/main.py
@@ -1189,6 +1189,23 @@ class Main:
         )
         self.calibrate_label.grid(row=0, columnspan=4, sticky=W + E)
 
+        # ----------------HEADING test-------------------
+        self.heading_test_button = Button(
+            self.calibrate_frame,
+            text = "Heading Test",
+            #takefocus = False,
+            padx = BUTTON_PAD_X,
+            pady = BUTTON_PAD_Y,
+            font = (FONT, BUTTON_SIZE),
+            command = lambda: self.out_q.put(
+                "test_heading()"
+            )
+        )
+
+        self.heading_test_button.grid(row=2, column=1, pady=CALIBRATE_PAD_Y)
+
+        #----------------------------------------------
+
         # test move forward
         self.forward_calibrate_button = Button(
             self.calibrate_frame,
