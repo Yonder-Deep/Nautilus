11267df89d40c8007f9a7d6fd9bd6d79bf88af9b
abijitj <ajayachandran@ucsd.edu>
2024-02-20 19:32:42 -0800

    Remap IMU axis and invert pitch and roll

--- auv/api/imu_tester.py
+++ auv/api/imu_tester.py
@@ -48,10 +48,18 @@ print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
 print('Reading BNO055 data, press Ctrl-C to quit...')
 while True:
     # Read the Euler angles for heading, roll, pitch (all in degrees).
+    bno.set_axis_remap(1, 0, 2, 1, 0, 0) 
+
     heading, roll, pitch = bno.read_euler()
+    roll = -1 * roll
+    pitch = -1 * pitch 
     # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
     sys, gyro, accel, mag = bno.get_calibration_status()
+
+    x, y, z, x_sign, y_sign, z_sign = bno.get_axis_remap(); 
     # Print everything out.
+
+    print(x, y, z, x_sign, y_sign, z_sign); 
     print('Heading={0:0.2F} Roll={1:0.2F} Pitch={2:0.2F}\tSys_cal={3} Gyro_cal={4} Accel_cal={5} Mag_cal={6}'.format(
           heading, roll, pitch, sys, gyro, accel, mag))
     # Other values you can optionally read:
