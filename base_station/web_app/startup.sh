18d7922baaae871018e88b6aa65fc47735235a8f
Kaz <80447192+miyatakazuya@users.noreply.github.com>
2024-04-29 23:15:53 -0700

    added comments

--- base_station/web_app/startup.sh
+++ base_station/web_app/startup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+
+# This script is used to start the web application for the Nautilus base station.
+
+# Start the Python Flask server by running the app.py script in the background.
 python app.py &
 
-
+# Change directory to the React/auv_gui folder.
 cd React/auv_gui
-npm build  &
-npm run start 
+
+# Build the React application in the background.
+npm build &
+
+# Start the React development server.
+npm run start
+
