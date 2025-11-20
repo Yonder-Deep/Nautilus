2764d229c7bd3ab0cfe908720a63b6b7ce06c5a8
abijitj <ajayachandran@ucsd.edu>
2025-11-19 20:18:07 -0800

    adding clock rate

--- web_app/video.py
+++ web_app/video.py
@@ -87,7 +87,7 @@ class VideoThread(threading.Thread):
             'gst-launch-1.0', 
             'udpsrc', 
             'port=5000', 
-            'caps=application/x-rtp,media=video,encoding-name=H264,payload=96',
+            'caps=application/x-rtp,media=video,clock-rate=90000,encoding-name=H264,payload=96',
             '!', 'rtph264depay', 
             '!', 'h264parse', 
             '!', 'avdec_h264', 
